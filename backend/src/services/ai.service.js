e51f41c0df75ada43207311b45229f3b7a547ddb
Tohid Ladman <tohidladman76@gmail.com>
2026-06-29 17:05:48 +0530

    update gemini model

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -148,7 +148,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-3.1-flash-lite",
+        model: "gemini-2.5-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
