6e0c026c725631df7104fdc4a9a2b996f578830f
Tohid Ladman <tohidladman76@gmail.com>
2026-06-29 10:05:31 +0530

    feat: add Interview component and update routing; enhance Home component layout and styles

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -3,6 +3,7 @@ import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
+import Interview from "./features/interview/pages/Interview";
 
 const ErrorPage = () => (
   <div>
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
     path: "/",
     element:<Protected><Home /></Protected>,
     // element: <Navigate to="/login" replace />
+  },
+  {
+        path:"/interview/:interviewId",
+        element: <Protected><Interview /></Protected>
   },
   {
     path: "/login",
