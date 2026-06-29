73196589b74d5730ca018f6ae16290539bcfe98f
Tohid Ladman <tohidladman76@gmail.com>
2026-06-29 16:30:40 +0530

    update cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interview-platform-bay-six.vercel.app",
     credentials: true,
   }),
 );
